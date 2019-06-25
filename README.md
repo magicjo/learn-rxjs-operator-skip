31ddcc28f28949020166e2b59411922c92bb476a
Johan GIRARD <magicjo@tiglab.net>
2019-06-25 20:20:31 +0200

    Improve example

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+[![Build Status](https://travis-ci.org/magicjo/learn-rxjs-operator-skip.svg?branch=master)](https://travis-ci.org/magicjo/learn-rxjs-operator-skip)
+
 # learn-rxjs-operator-skip
 
+Example about the filtering operator `skip`.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learn-rxjs-operator-skip)
+
+## How to execute in local ?
+
+Execute `src/index.ts`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest start
+```
+
+Execute `tslint`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest lint
+```
--- index.ts
+++ index.ts
@@ -1,9 +1,29 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+// RxJS v6+
+import { range } from "rxjs";
+import { skip } from "rxjs/operators";
 
+/*
+ Filtering Operator: skip
+ ------------------------
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+ Skip the **count** number of emitted values.
+ ```
+ skip(count: number): Observable
+ ```
 
-source.subscribe(x => console.log(x));
+ http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-skip
+*/
+
+// Emit: 0, 1, 2, ..., 8
+const source = range(9);
+
+// Skip the first 6 emitted values
+const example = source.pipe(skip(6));
+
+// Receive: 6, 7, 8
+console.log("[start]");
+example.subscribe({
+  complete: () => console.log("[complete]"),
+  error: err => console.error("[error] : ", err),
+  next: value => console.log("[next] : ", value)
+});
